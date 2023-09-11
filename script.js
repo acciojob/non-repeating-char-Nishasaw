8659e465c1db6cee13be298a0168b70af07c665e
Nishasaw <139792337+Nishasaw@users.noreply.github.com>
2023-09-12 00:46:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const charFrequency = {};
+
+  // Iterate through the string to count character occurrences
+  for (const char of str) {
+    charFrequency[char] = (charFrequency[char] || 0) + 1;
+  }
+
+  // Iterate through the string again to find the first non-repeated character
+  for (const char of str) {
+    if (charFrequency[char] === 1) {
+      return char; // Return the first non-repeated character
+    }
+  }
+
+  return null; // Return null if no non-repeated character is found
+
 }
+console.log(firstNonRepeatedChar('aabbcdd')); // Output: 'c'
+console.log(firstNonRepeatedChar('aabbcc'));
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
